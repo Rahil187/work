1ccb05cccd1a3ebbf97480fceac1c7858115f749
Rahil187 <127247511+Rahil187@users.noreply.github.com>
2023-03-09 15:45:42 +0000

    Add files via upload

---
+++ create.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Opened database successfully");
+conn.execute('CREATE TABLE users (username TEXT, password TEXT)')
+print("Table created successfully")
+conn.close()
